a7f0b7f4a1fd5ace11f816bc120e11f28fe3f954
fumiyadoi <fdoi1996@gmail.com>
2023-05-24 18:23:51 +0900

    feature: titleとメタ追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,13 @@ import { defineNuxtConfig } from "nuxt/config";
 export default defineNuxtConfig({
   css: ["~/assets/styles/tailwind.css"],
   modules: ["@nuxtjs/tailwindcss"],
+  app: {
+    head: {
+      title: "フルーツショップ",
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+      ],
+    },
+  },
 });
