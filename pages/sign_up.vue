0e76a7f721d017a74081701497bad99e1619a29f
fumiyadoi <fdoi1996@gmail.com>
2023-05-24 20:59:46 +0900

    chore

--- pages/sign_up.vue
+++ pages/sign_up.vue
@@ -1,7 +1,7 @@
 <template>
   <NuxtLayout>
     <div
-      class="h-[calc(100vh_-_44px)] flex flex-col justify-center items-center"
+      class="h-[calc(100vh_-_44px)] flex flex-col justify-center items-center px-3"
     >
       <SignUpForm />
     </div>
