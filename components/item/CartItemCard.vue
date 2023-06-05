2ac1c72e550e8396f7799a2a750ba1bcc9e89f03
fumiyadoi <fdoi1996@gmail.com>
2023-06-05 18:25:45 +0900

    chore

--- components/item/CartItemCard.vue
+++ components/item/CartItemCard.vue
@@ -1,26 +1,26 @@
 <template>
   <div class="md:h-[240px] h-[120px] w-full bg-white flex">
-    <img :src="itemCount.image" class="object-cover h-full" alt="apple" />
+    <img :src="cartItem.image" class="object-cover h-full" alt="apple" />
     <div class="w-full flex flex-col justify-between m-6">
       <div class="flex justify-between items-center">
-        <div class="text-lg">{{ itemCount.name }}</div>
+        <div class="text-lg">{{ cartItem.name }}</div>
         <div class="text-xl font-bold text-red-500">
-          ¥{{ itemCount.price.toLocaleString() }}
+          ¥{{ cartItem.price.toLocaleString() }}
         </div>
       </div>
       <div class="flex justify-between items-center">
         <div class="flex items-center gap-4">
           <div class="whitespace-nowrap">数量</div>
           <Select
-            :item="itemCount.count.toString()"
+            :item="cartItem.count.toString()"
             :items="items"
-            @update:item="(item) => onChangeSelect(item, itemCount.id)"
+            @update:item="(item) => onChangeSelect(item, cartItem.id)"
           />
         </div>
         <Button
           text="削除する"
           type="danger"
-          :onClick="() => deleteItem(itemCount.id)"
+          :onClick="() => deleteItem(cartItem.id)"
         />
       </div>
     </div>
@@ -33,7 +33,7 @@ import { CartItem } from "~/composables/useCartItems";
 const { changeCount, deleteItem } = useCartItems();
 
 interface Props {
-  itemCount: CartItem;
+  cartItem: CartItem;
 }
 
 const Props = defineProps<Props>();
--- pages/cart.vue
+++ pages/cart.vue
@@ -3,7 +3,7 @@
     <div class="relative">
       <div class="flex flex-col items-center pt-6 pb-20">
         <div class="md:w-[768px] w-full flex flex-col gap-4 px-3">
-          <CartItemCard v-for="itemCount in cartItems" :itemCount="itemCount" />
+          <CartItemCard v-for="cartItem in cartItems" :cartItem="cartItem" />
         </div>
       </div>
       <div class="bottom-0 left-0 right-0 fixed">
